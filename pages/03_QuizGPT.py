d1bc9aa5318af4b6ee6803740c3e11f2afbb4bae
HyunJun Baek <hjbaek5796@gmail.com>
2024-04-04 16:21:25 +0900

    [QuizGPT] Set up starting code for QuizGPT

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
+st.set_page_config(
+  page_title="QuizGPT",
+  page_icon="❓",
+)
+
 st.title("QuizGPT")
+
+with st.sidebar:
+  choice = st.selectbox(
+    "Choose what you want to use.", 
+    (
+      "File",
+      "Wikipedia Article", 
+    ),
+  )
+  if choice == "File":
+    file = st.file_uploader(
+      "Upload a .docx, .txt or .pdf file",
+      type=["pdf", "txt", "docx"],
+    )
+  else:
+    topic = st.text_input("Name of the article")
