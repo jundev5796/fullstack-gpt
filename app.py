d0e7fb5d5dd5c38937b23b5f40825ecced6d9114
HyunJun Baek <hjbaek5796@gmail.com>
2024-04-04 18:57:50 +0900

    DocumentGPT challenge start

---
+++ app.py
@@ -0,0 +1,15 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
+import streamlit as st
+
+st.set_page_config(
+  page_title="DocumentGPT",
+  page_icon="📃",
+)
